d8900090cb1f15681cd1e8f1101ff61d9f3d27cf
denisval67 <valievdenis265@gmail.com>
2024-12-11 15:32:14 +0800

    каркас приложения

---
+++ WpfApp1/hotelspage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для hotelspage.xaml
+    /// </summary>
+    public partial class hotelspage : Page
+    {
+        public hotelspage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            manager.Mainframe.Navigate(new addeditpage());
+        }
+    }
+}
